a3b30b9a06288e73494b6df29e61a74630656103
aryareji <37616393+aryareji@users.noreply.github.com>
2019-02-04 12:17:19 +0530

    Add files via upload

--- number.rb
+++ number.rb
@@ -2,7 +2,7 @@ puts "Enter first number"
 a = gets.to_i
 puts "Enter Second number"
 b = gets.to_i
-a > b ? c = a * b : d = a + b
+a > b ? c = a * b : c = a + b
 puts "Result is #{c}"
-puts "Result is #{d}"
+
 
--- student1.rb
+++ student1.rb
@@ -1,6 +1,7 @@
 
 
 class Student
+@@no_stud =0
    def initialize(name, rollno, mark1, mark2)
       @name = name
       @rollno = rollno
@@ -13,11 +14,14 @@ class Student
       puts "Student Name is #@name"
       puts "Subject 1 mark is #@mark1"
       puts "Subject 2 mark is #@mark2"
-     
+     @@no_stud += 1
    end
    def avg()
       puts "Average mark of #@name is #{(@mark1 + @mark2) / 2}"
    end
+def total_no_stud()
+puts "Total number of students is  #@@no_stud"
+end 
 end
 stud1= Student.new("devika",11,15,40)
 stud2= Student.new("Arya",7,20,30)
@@ -30,3 +34,5 @@ stud3.display_details()
 stud1.avg()
 stud2.avg()
 stud3.avg()
+stud1.total_no_stud
+
